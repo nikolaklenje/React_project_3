a131b889f54490c622de4c7312bac6984ae0b1a8
nikolaklenje <nikolaelite88@gmail.com>
2022-09-22 17:04:13 -0400

    New

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <div className="Header">
+          <div className="LeftHeader"> 
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png" alt=""></img>
+            <h3>MemeGenerator</h3>
+          </div>
+          <p>React Course - Project 3</p>
+
+        </div>
+
+    )
+}
