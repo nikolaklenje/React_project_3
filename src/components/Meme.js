9faf4d19e01670461e59f69af63ca4912c5be004
nikolaklenje <nikolaelite88@gmail.com>
2022-09-24 16:54:53 -0400

    Meme

---
+++ src/components/Meme.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Meme(){
+    return (
+       <div className="Meme">
+         <div>
+            <div className="InputForm">
+             <input type="text" placeholder="Shut up"></input>
+             <input type="text" placeholder="and take my money"></input></div>
+           <button><p className="ButtonText">Get a new meme image</p></button>
+         </div>
+          </div>
+       
+    )
+}
