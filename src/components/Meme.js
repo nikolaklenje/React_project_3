4e45bc2b184f2a84566687273981f723edbdca02
nikolaklenje <nikolaelite88@gmail.com>
2022-09-24 17:18:04 -0400

    Meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,14 +2,14 @@ import React from "react"
 
 export default function Meme(){
     return (
-       <div className="Meme">
-         <div>
-            <div className="InputForm">
+       <main className="Meme">
+         
+            <form className="InputForm">
              <input type="text" placeholder="Shut up"></input>
-             <input type="text" placeholder="and take my money"></input></div>
+             <input type="text" placeholder="and take my money"></input></form>
            <button><p className="ButtonText">Get a new meme image</p></button>
-         </div>
-          </div>
+         
+          </main>
        
     )
 }
