a9e558bf23c5d52eb93c308a6f3b4abf81edfa02
nikolaklenje <nikolaelite88@gmail.com>
2022-09-27 21:33:30 -0400

    MIss

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,28 @@
 import React from "react"
+import memesData from "../memesData.js"
+
+//  create default function that returns elements for Main part of page,
+
+
 
 export default function Meme(){
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.lenght)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+
+    }
     return (
+ 
+
        <main className="Meme">
          
-            <form className="InputForm">
+            <div className="InputForm">
              <input type="text" placeholder="Shut up"></input>
-             <input type="text" placeholder="and take my money"></input></form>
-           <button><p className="ButtonText">Get a new meme image</p></button>
+             <input type="text" placeholder="and take my money"></input></div>
+           <button onClick={getMemeImage}><p className="ButtonText">Get a new meme image</p></button>
          
           </main>
        
